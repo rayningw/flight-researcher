d8962e6c292fbee6e314ade43af07fb491f21891
Ray Wu <rwu@quantcast.com>
2015-02-08 18:21:44 -0800

    Ability to RUD

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.get('/api/v1/airport_pairs', function(req, res) {
-  var pairs = JSON.parse(fs.readFileSync('data/airport_pairs.json'));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    rows: pairs
-  }));
-});
+function addPairHandlers(url, jsonFile) {
+  app.get(url, function(req, res) {
+    console.log('Reading: ' + jsonFile);
+    var pairs = JSON.parse(fs.readFileSync(jsonFile));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      rows: pairs
+    }));
+  });
 
-app.get('/api/v1/date_pairs', function(req, res) {
-  var pairs = JSON.parse(fs.readFileSync('data/date_pairs.json'));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    rows: pairs
-  }));
-});
+  app.post(url, function(req, res) {
+    console.log('Writing: ' + jsonFile);
+    var pairs = req.body.rows;
+    console.log('Body: ' + JSON.stringify(pairs));
+    fs.writeFile(jsonFile, JSON.stringify(pairs));
+    res.send();
+  });
+}
+
+addPairHandlers('/api/v1/airport_pairs', 'data/airport_pairs.json');
+addPairHandlers('/api/v1/date_pairs', 'data/date_pairs.json');
 
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,6 +9,14 @@ var DATE_PAIRS_URL = "/api/v1/date_pairs";
 
 var ParameterBox = React.createClass({
 
+  getInitialState: function() {
+    return {rows: []};
+  },
+
+  componentDidMount: function() {
+    this.loadTable();
+  },
+
   loadTable: function() {
     console.log('Loading table for: ' + this.props.label);
 
@@ -24,21 +32,99 @@ var ParameterBox = React.createClass({
     });
   },
 
-  getInitialState: function() {
-    return {rows: []};
+  saveTable: function(rows) {
+    console.log('Saving table for: ' + this.props.label);
+
+    $.ajax({
+      url: this.props.url,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        rows: rows
+      }),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
 
-  componentDidMount: function() {
-    this.loadTable();
+  handleUpdate: function(newRows) {
+    console.log('Saving: ' + this.props.label + ' with:', newRows);
+    this.setState({rows: newRows});
+    this.saveTable(newRows);
   },
 
   render: function() {
     return (
       <div className="parameter-box">
         <div className="label">{this.props.label}</div>
-        <Table headings={this.props.headings} rows={this.state.rows} />
+        <Table headings={this.props.headings} rows={this.state.rows} onUpdate={this.handleUpdate} />
       </div>
     );
+  }
+
+});
+
+var TableRow = React.createClass({
+
+  getInitialState: function() {
+    return {isEdit: false}
+  },
+
+  handleEdit: function(event) {
+    assert(!this.state.isEdit);
+    this.setState({isEdit: true});
+  },
+
+  handleSave: function(event) {
+    assert(this.state.isEdit);
+
+    var newValues = [
+      this.refs.value1.getDOMNode().value.trim(),
+      this.refs.value2.getDOMNode().value.trim()
+    ];
+
+    if (!newValues[0] || !newValues[1]) {
+      return;
+    }
+    
+    this.setState({isEdit: false});
+    this.props.onUpdate(newValues);
+  },
+
+  handleDelete: function(event) {
+    assert(!this.state.isEdit);
+    this.props.onDelete();
+  },
+
+  render: function() {
+    if (this.state.isEdit) {
+      return (
+        <div className="row">
+          <div style={{width: this.props.colWidth}}>
+            <input ref="value1" type="text" defaultValue={this.props.values[0]}></input>
+          </div>
+          <div style={{width: this.props.colWidth}}>
+            <input ref="value2" type="text" defaultValue={this.props.values[1]}></input>
+          </div>
+          <div className="actions" style={{width: this.props.actionsColWidth}}>
+            <a href="#" className="action" onClick={this.handleSave}>Save</a>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="row">
+          <div ref="col1" style={{width: this.props.colWidth}}>{this.props.values[0]}</div>
+          <div ref="col2" style={{width: this.props.colWidth}}>{this.props.values[1]}</div>
+          <div className="actions" style={{width: this.props.actionsColWidth}}>
+            <a href="#" className="action" onClick={this.handleEdit}>Edit</a>
+            <a href="#" className="action" onClick={this.handleDelete}>Delete</a>
+          </div>
+        </div>
+      );
+    }
   }
 
 });
@@ -47,24 +133,41 @@ var Table = React.createClass({
 
   render: function() {
     assert(this.props.headings.length == 2);
+
+    var ACTIONS_COL_WIDTH = 100;
     
     var width = this.props.width || 300;
     var colWidth = width / 2;
 
-    var rowNodes = this.props.rows.map(function(row) {
-      return (
-        <div className="row">
-          <div style={{width: colWidth}}>{row.values[0]}</div>
-          <div style={{width: colWidth}}>{row.values[1]}</div>
-        </div>
-      );
-    });
+    var rowNodes = this.props.rows.map(function(row, idx) {
+      var handleUpdate = function(newValues) {
+        var newRows = this.props.rows.slice();
+        newRows[idx] = {
+          values: newValues
+        };
+        this.props.onUpdate(newRows);
+      }.bind(this);
+
+      var handleDelete = function() {
+        var newRows = this.props.rows.slice();
+        newRows.splice(idx, 1);
+        this.props.onUpdate(newRows);
+      }.bind(this);
+
+      return <TableRow key={idx}
+                       colWidth={colWidth}
+                       actionsColWidth={ACTIONS_COL_WIDTH}
+                       values={row.values}
+                       onUpdate={handleUpdate}
+                       onDelete={handleDelete} />
+    }.bind(this));
 
     return (
       <div className="table">
         <div className="header row">
           <div style={{width: colWidth}}>{this.props.headings[0]}</div>
           <div style={{width: colWidth}}>{this.props.headings[1]}</div>
+          <div style={{width: ACTIONS_COL_WIDTH}}>&nbsp;</div>
         </div>
 
         {rowNodes}
